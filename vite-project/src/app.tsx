1597935fed9e6abeeeccdfa3687e41aa85187161
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-09-08 08:22:30 +0300

    feat: added a Timer mobx class

--- vite-project/src/app.tsx
+++ vite-project/src/app.tsx
@@ -1,43 +1,27 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import React from "react";
+import { makeAutoObservable } from "mobx";
+import { observer } from "mobx-react-lite";
 
-export function App() {
-  const [count, setCount] = useState(0)
+class Timer {
+  secondsPassed = 0;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  increaseTimer() {
+    this.secondsPassed += 1;
+  }
 }
+
+const myTimer = new Timer();
+
+const TimerView = observer(({ timer }: { timer: Timer }) => (
+  <span>Seconds passed: {timer.secondsPassed}</span>
+));
+
+function App() {
+  return <TimerView timer={myTimer} />;
+}
+
+export { App };
