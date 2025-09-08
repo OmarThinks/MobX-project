a35ff1746dfe0fef89f5ced9bd40c15cb9ab7cb5
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-09-08 08:26:04 +0300

    feat: timer now works automatically

--- vite-project/src/app.tsx
+++ vite-project/src/app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { makeAutoObservable } from "mobx";
 import { observer } from "mobx-react-lite";
 
@@ -16,9 +16,20 @@ class Timer {
 
 const myTimer = new Timer();
 
-const TimerView = observer(({ timer }: { timer: Timer }) => (
-  <span>Seconds passed: {timer.secondsPassed}</span>
-));
+const TimerView = observer(() => {
+  const [timer] = useState(() => new Timer()); // See the Timer definition above.
+
+  useEffect(() => {
+    const handle = setInterval(() => {
+      timer.increaseTimer();
+    }, 1000);
+    return () => {
+      clearInterval(handle);
+    };
+  }, [timer]);
+
+  return <span>Seconds passed: {timer.secondsPassed}</span>;
+});
 
 function App() {
   return <TimerView timer={myTimer} />;
